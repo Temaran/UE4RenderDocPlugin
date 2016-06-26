f36934d0d4d0d24f705467e6c237f2463958357c
Marcos Slomp <mslomp@gmail.com>
2016-06-26 21:17:30 +0900

    toolbar widget embellishments based on STransformViewportToolbar::MakeSurfaceSnappingButton()

--- RenderDocPlugin/Source/RenderDocPlugin/Private/RenderDocPluginSettingsEditorWindow.cpp
+++ RenderDocPlugin/Source/RenderDocPlugin/Private/RenderDocPluginSettingsEditorWindow.cpp
@@ -83,6 +83,17 @@ public:
 
 };
 
+#include "EditorStyleSet.h"
+
+namespace TransformViewportToolbarDefs
+{
+  /** Size of the arrow shown on SGridSnapSettings Menu button */
+  const float DownArrowSize = 4.0f;
+
+  /** Size of the icon displayed on the toggle button of SGridSnapSettings */
+  const float ToggleImageScale = 16.0f;
+}
+
 void SRenderDocPluginSettingsEditorWindow::Construct(const FArguments& InArgs)
 {
   auto ThePlugin = InArgs._ThePlugin;
@@ -92,76 +103,128 @@ void SRenderDocPluginSettingsEditorWindow::Construct(const FArguments& InArgs)
   BindCommands(RenderDocSettings);
 
   FSlateIcon IconBrush = FSlateIcon(FRenderDocPluginStyle::Get()->GetStyleSetName(), "RenderDocPlugin.CaptureFrameIcon.Small");
-  //FSlateIcon SettingsIconBrush = FSlateIcon(FRenderDocPluginStyle::Get()->GetStyleSetName(), "RenderDocPlugin.SettingsIcon.Small");
+  FSlateIcon SettingsIconBrush = FSlateIcon(FRenderDocPluginStyle::Get()->GetStyleSetName(), "RenderDocPlugin.SettingsIcon.Small");
 
-  static bool clicked (false);
+  // Widget inspired by STransformViewportToolbar::MakeSurfaceSnappingButton()
+  FName ToolBarStyle = TEXT("ViewportMenu");
   ChildSlot
   [
-    SNew(SViewportToolBarComboMenu)
-    .Cursor(EMouseCursor::Default)
-    .Style(TEXT("ViewportMenu"))
-    .IsChecked_Lambda([]()->ECheckBoxState { return(ECheckBoxState::Unchecked); })
-    .OnCheckStateChanged_Lambda([ThePlugin](ECheckBoxState InState)
-    {
-      //FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
-      //TSharedRef<FUICommandList> CommandBindings = LevelEditorModule.GetGlobalLevelEditorActions();
-      //CommandBindings->GetActionForCommand(FRenderDocPluginCommands::Get().CaptureFrame)->Execute();
-      ThePlugin->CaptureFrame();
-    })
-    //.Label_Lambda([]()->FText { return(FText::Format(LOCTEXT("GridRotation - Number - DegreeSymbol", "{0}\u00b0"), FText::AsNumber(GEditor->GetRotGridSize().Pitch))); })
-    .ToggleButtonToolTip(LOCTEXT("RenderDocCapture_ToolTipOverride", "Captures the next frame and launches RenderDoc."))  // TODO: aquire tooltip captions from the CaptureFrame command
-    .MenuButtonToolTip(LOCTEXT("RenderDocSettings_ToolTipOverride", "RenderDoc Settings"))
-    .Icon(IconBrush)
-    .ParentToolBar(SharedThis(this))
-    .OnGetMenuContent_Lambda([this,RenderDocSettings]() -> TSharedRef<SWidget>
-    {
-      auto& Commands = FRenderDocSettingsCommands::Get();
-      FMenuBuilder ShowMenuBuilder (true, CommandList);
+    SNew(SHorizontalBox)
 
-      for (auto& command : Commands.Commands)
-        ShowMenuBuilder.AddMenuEntry(command);
+    + SHorizontalBox::Slot()
+    .AutoWidth()
+    [
+      SNew(SButton)
+      .ToolTipText(LOCTEXT("RenderDocCapture_ToolTipOverride", "Captures the next frame and launches RenderDoc."))  // TODO: aquire tooltip captions from the CaptureFrame command
+      .OnClicked_Lambda([ThePlugin]() { ThePlugin->CaptureFrame(); return(FReply::Handled()); })
+      [
+        SNew(SImage)
+        .Image(IconBrush.GetIcon())
+      ]
+    ]
 
-      ShowMenuBuilder.AddWidget(
-        SNew(SVerticalBox)
-        +SVerticalBox::Slot()
-        .AutoHeight()
-        [
-          SNew(SHorizontalBox)
-          + SHorizontalBox::Slot()
-          .AutoWidth()
-          .VAlign(EVerticalAlignment::VAlign_Center)
-          .Padding(5)
-          [
-            SNew(SButton)
-            .Text(LOCTEXT("SaveButton", "Save"))
-            .OnClicked_Lambda([RenderDocSettings]()
-            {
-              RenderDocSettings->Save();
-              return( FReply::Handled() );
-            })
-          ]
+    + SHorizontalBox::Slot()
+    .AutoWidth()
+    [
+      SNew(SCheckBox)
+      .Cursor(EMouseCursor::Default)
+      .Style(FEditorStyle::Get(), EMultiBlockLocation::ToName(FEditorStyle::Join(ToolBarStyle, ".ToggleButton"), EMultiBlockLocation::End))
+      .Padding(0)
+      .ToolTipText(LOCTEXT("RenderDocSettings_ToolTipOverride", "RenderDoc Settings"))
+      .IsChecked_Static([] { return(ECheckBoxState::Unchecked); })
+      .Content()
+			[
+				SNew( SComboButton )
+				.ButtonStyle( FEditorStyle::Get(), "HoverHintOnly" )
+				.HasDownArrow( false )
+				.ContentPadding( 0 )
+				.ButtonContent()
+				[
+					SNew( SVerticalBox )
 
-          +SHorizontalBox::Slot()
-          .AutoWidth()
-          .VAlign(EVerticalAlignment::VAlign_Center)
-          .Padding(5)
-          [
-            SNew(SButton)
-            .Text(LOCTEXT("AboutButton", "About"))
-            .OnClicked_Lambda([]()
-            {
-              GEditor->EditorAddModalWindow( SNew(SRenderDocPluginAboutWindow) );
-              return( FReply::Handled() );
-            })
-          ]
-        ],
-        FText()
-      );
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					.Padding(FMargin(3.f, 2.f, 5.f, 0.f))
+					[
+						SNew( SBox )
+						.WidthOverride( TransformViewportToolbarDefs::ToggleImageScale )
+						.HeightOverride( TransformViewportToolbarDefs::ToggleImageScale )
+						.HAlign( HAlign_Center )
+						.VAlign( VAlign_Center )
+						[
+							SNew( SImage )
+							.Image( SettingsIconBrush.GetIcon() )
+						]
+					]
 
-      return(ShowMenuBuilder.MakeWidget());
-    })
-  ];
+					+ SVerticalBox::Slot()
+					.AutoHeight()
+					.HAlign( HAlign_Center )
+					.Padding(FMargin(0.f, 0.f, 0.f, 3.f))
+					[
+						SNew( SBox )
+						.WidthOverride( TransformViewportToolbarDefs::DownArrowSize )
+						.HeightOverride( TransformViewportToolbarDefs::DownArrowSize )
+						[
+							SNew(SImage)
+							.Image(FEditorStyle::GetBrush("ComboButton.Arrow"))
+							.ColorAndOpacity(FLinearColor::Black)
+						]
+					]
+				]
+				.MenuContent()
+				[        
+          ([this,RenderDocSettings]() -> TSharedRef<SWidget>
+          {
+            auto& Commands = FRenderDocSettingsCommands::Get();
+            FMenuBuilder ShowMenuBuilder (true, CommandList);
 
+            for (auto& command : Commands.Commands)
+              ShowMenuBuilder.AddMenuEntry(command);
+
+            ShowMenuBuilder.AddWidget(
+              SNew(SVerticalBox)
+              +SVerticalBox::Slot()
+              .AutoHeight()
+              [
+                SNew(SHorizontalBox)
+                + SHorizontalBox::Slot()
+                .AutoWidth()
+                .VAlign(EVerticalAlignment::VAlign_Center)
+                .Padding(5)
+                [
+                  SNew(SButton)
+                  .Text(LOCTEXT("SaveButton", "Save"))
+                  .OnClicked_Lambda([RenderDocSettings]()
+                  {
+                    RenderDocSettings->Save();
+                    return( FReply::Handled() );
+                  })
+                ]
+
+                +SHorizontalBox::Slot()
+                .AutoWidth()
+                .VAlign(EVerticalAlignment::VAlign_Center)
+                .Padding(5)
+                [
+                  SNew(SButton)
+                  .Text(LOCTEXT("AboutButton", "About"))
+                  .OnClicked_Lambda([]()
+                  {
+                    GEditor->EditorAddModalWindow( SNew(SRenderDocPluginAboutWindow) );
+                    return( FReply::Handled() );
+                  })
+                ]
+              ],
+              FText()
+            );
+
+            return(ShowMenuBuilder.MakeWidget());
+          }())
+        ] // end of SComboButton::MenuContent()
+      ] // end of SComboButton
+    ] // end of SCheckBox
+  ];  // end of Toolbar extension
 }
 
 void SRenderDocPluginSettingsEditorWindow::BindCommands(FRenderDocPluginSettings* Settings)
