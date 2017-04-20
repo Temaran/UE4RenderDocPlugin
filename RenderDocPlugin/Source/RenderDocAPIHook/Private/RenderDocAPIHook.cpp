123bfdafc54d51aaf6f629e0832436a3fff8ea56
babymustgrowstrong <babymustgrowstrong@yahoo.com>
2017-04-19 19:57:47 -0700

    RenderDocAPIHook

---
+++ RenderDocPlugin/Source/RenderDocAPIHook/Private/RenderDocAPIHook.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+* The MIT License (MIT)
+*
+* Copyright (c) 2017 Jimmy Lee
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+******************************************************************************/
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+#include "Modules/ModuleInterface.h"
+
+
+class FRenderDocAPIHook : public IModuleInterface
+{
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+IMPLEMENT_MODULE(FRenderDocAPIHook, RenderDocAPIHook)
+
+void FRenderDocAPIHook::StartupModule()
+{
+
+}
+
+void FRenderDocAPIHook::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
